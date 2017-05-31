cc6b6f92eed10a4c6bd96c500ceaec65af901cb6
Donrockz <rockstarrockey18@gmail.com>
2017-05-31 23:15:03 +0530

    Add files via upload

---
+++ Assignment1.3/src/assignment3/ass3.java
@@ -0,0 +1,25 @@
+package assignment3;
+
+public class ass3 {
+
+	public static void main(String[] args) {
+		
+		int a = 12;
+		int b = 7;
+		
+		System.out.println("Before swapping a = "+ a);
+		System.out.println("Before swapping b = "+ b);
+		
+		a= a+b;
+		b= a-b;
+		a=a-b;
+		
+		System.out.println("After swapping a = "+ a);
+		System.out.println("After swapping b = "+ b);
+		
+		// This assignment is not done out of my own creation. The architecture of this code is copied and I myself practiced the same. Please excuse me
+		
+		
+				
+	}
+}
